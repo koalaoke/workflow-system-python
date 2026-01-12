54c9773a9ef48ae518bee0bfc2628d9e1f583ee5
Bernardo Brito <koalaokeah@outlook.com>
2026-01-11 21:39:27 -0300

    Atuliza CLI para usar rich e argparse

--- src/ui/cli.py
+++ src/ui/cli.py
@@ -1,76 +1,40 @@
-import time
+import shlex
+import argparse
+from .process import process_parser
 from rich.console import Console
 from src.services.manager import GerenciadorDeProcessos
-from src.domain.exceptions import TransicaoInvalidaError
 
-# Instância do sistema
 sistema = GerenciadorDeProcessos()
-
-def pausar():
-    input("\n[ENTER] para continuar...")
-
-def menu_processo(console ,processo):
-    while True:
-        console.clear()
-        console.print(f"🔧 GERENCIANDO: {processo.titulo}")
-        console.print(f"🚦 ESTADO: [{processo.estado_atual}]")
-        console.print("1. ✅ Aprovar / Avançar")
-        console.print("2. ⛔ Rejeitar")
-        console.print("3. 📜 Ver Histórico")
-        console.print("0. 🔙 Voltar")
-        
-        op = console.input("\nOpção: ")
-        
-        try:
-            if op == "1":
-                processo.aprovar()
-                console.print(">> Sucesso: Processo avançou.")
-                time.sleep(1)
-            elif op == "2":
-                processo.rejeitar()
-                console.print(">> Sucesso: Processo rejeitado.")
-                time.sleep(1)
-            elif op == "3":
-                processo.ver_historico()
-                pausar()
-            elif op == "0":
-                break
-            else:
-                console.print("Opção inválida.")
-                time.sleep(0.5)
-        except TransicaoInvalidaError as e:
-            console.print(f"\n❌ ERRO DE REGRA: {e}")
-            pausar()
 
 def menu_principal():
     sistema.criar("Aquisição de Notebooks")
     
     console = Console()
     while True:
-        console.print()
-        console.clear()
-        print("=== SISTEMA WORKFLOW ===")
-        lista = sistema.listar()
         
-        for i, p in enumerate(lista):
-            console.print(f"{i+1}. [{p.estado_atual}] {p.titulo}")
-            
-        print("-" * 30)
-        print("N. Novo Processo")
-        print("0. Sair")
-        
-        op = console.input("\nEscolha (Número ou Letra): ").upper()
-        
-        if op == "0":
-            break
-        elif op == "N":
-            titulo = input("Nome do processo: ")
-            if titulo: sistema.criar(titulo)
-        elif op.isdigit():
-            idx = int(op)
-            proc = sistema.buscar(idx)
-            if proc:
-                menu_processo(console, proc)
-            else:
-                print("Processo não encontrado.")
-                time.sleep(1)
+        raw_input = console.input("(workflow) >")
+        user_command = shlex.split(raw_input)
+
+        match user_command[0]:
+            case "help":
+                console.print("Workflow - Seus sistema de processos.")
+                console.print("help - Exibe ajuda.")
+                console.print("process list- Exibe os processos.")
+                console.print("process new - Cria processo.")
+                console.print("process <id> - Exibe um processo.")
+                console.print("clear - Limpa o console.")
+                console.print("exit - sai do sistema.")
+
+            case "process":
+                process_parser.set_defaults(sistema=sistema, console=console)
+                args = process_parser.parse_args(user_command[1:])
+                args.func(args)
+
+            case "clear":
+                console.clear()
+                console.print()
+
+            case "exit":
+                break
+            case _:
+                print("Comando não reconhecido. Use ")
---
+++ src/ui/process.py
@@ -0,0 +1,69 @@
+import argparse
+import time
+from rich.prompt import Prompt
+from src.domain.exceptions import TransicaoInvalidaError
+
+def pausar():
+    input("\n[ENTER] para continuar...")
+
+def list_process(args):
+    lista = args.sistema.listar()
+        
+    for i, p in enumerate(lista):
+        args.console.print(f"{i+1}. [{p.estado_atual}] {p.titulo}")
+
+def create_process(args):
+    titulo = Prompt.ask("Nome do processo: ")
+    if titulo: args.sistema.criar(titulo)
+
+def open_process(args):
+    idx = int(args.id)
+    processo = args.sistema.buscar(idx)
+    if not processo:
+        print("Processo não encontrado.")
+        time.sleep(1)
+        return
+    console = args.console
+    while True:
+        console.print(f"🔧 GERENCIANDO: {processo.titulo}")
+        console.print(f"🚦 ESTADO: [{processo.estado_atual}]")
+        console.print("1. ✅ Aprovar / Avançar")
+        console.print("2. ⛔ Rejeitar")
+        console.print("3. 📜 Ver Histórico")
+        console.print("0. 🔙 Voltar")
+        
+        op = console.input("\nOpção: ")
+        
+        try:
+            if op == "1":
+                processo.aprovar()
+                console.print(">> Sucesso: Processo avançou.")
+                time.sleep(1)
+            elif op == "2":
+                processo.rejeitar()
+                console.print(">> Sucesso: Processo rejeitado.")
+                time.sleep(1)
+            elif op == "3":
+                processo.ver_historico()
+                pausar()
+            elif op == "0":
+                break
+            else:
+                console.print("Opção inválida.")
+                time.sleep(0.5)
+        except TransicaoInvalidaError as e:
+            console.print(f"\n❌ ERRO DE REGRA: {e}")
+            pausar()
+
+process_parser = argparse.ArgumentParser(prog='process')
+subparsers = process_parser.add_subparsers(help='subcommand help')
+
+parser_new = subparsers.add_parser('new', help='new help')
+parser_new.set_defaults(func=create_process)
+
+parser_list = subparsers.add_parser('list', help='list help')
+parser_list.set_defaults(func=list_process)
+
+parser_open = subparsers.add_parser('open', help='open help')
+parser_open.add_argument("id")
+parser_open.set_defaults(func=open_process)
